85ab377db99ad38e0892b8432486c306dfa627bc
Sergei <ansemal@yandex.ru>
2020-02-07 23:56:30 +0200

    H6 Домашнее задание

--- src/ru/progwards/java1/lessons/arrays/DIntArray.java
+++ src/ru/progwards/java1/lessons/arrays/DIntArray.java
@@ -12,41 +12,30 @@ public class DIntArray {
         int [] mas1 = Arrays.copyOf(mas,mas.length+1);
         mas1 [mas.length] = num;
         mas = Arrays.copyOf(mas1,mas1.length);
-          //System.out.println(Arrays.toString(mas));
     }
 
     public void atInsert(int pos, int num) {
         int [] mas2 = new int [mas.length+1];
         for (int i=0; i<mas.length; i++) {
-            if (i < pos-1)
-                mas2[i] = mas[i];
+            if (i < pos) mas2[i] = mas[i];
             else mas2 [i + 1] = mas[i];
         }
-        mas2 [pos-1] = num;
+        mas2 [pos] = num;
         mas = Arrays.copyOf(mas2,mas2.length);
-          //System.out.println(Arrays.toString(mas));
     }
 
     public void atDelete(int pos) {
-        if (mas.length > 0) {
             int[] mas3 = new int[mas.length - 1];
             for (int i = 0; i < mas.length - 1; i++) {
-                if (i < pos) {
-                    mas3[i] = mas[i];}
-                else if (i > pos)
-                    mas3[i - 1] = mas[i];
+                if (i < pos)  mas3[i] = mas[i];
+                else if (i > pos)   mas3[i - 1] = mas[i];
                 else continue;
             }
             mas = Arrays.copyOf(mas3, mas3.length);
-                //System.out.println(Arrays.toString(mas));
-        }
     }
 
     public int at(int pos) {
-          //System.out.println(Integer.toString(mas[pos-1]));
-          if (pos > 1 && pos <= mas.length-1)
-        return mas[pos-1];
-          else return 0;
+        return mas[pos];
     }
 
     public static void main(String[] args) {
@@ -55,11 +44,8 @@ public class DIntArray {
         b.add(8);
         b.add(-48);
         b.add(0);
-        for (int i=0; i<20; i++)
-            b.add(i);
         b.atInsert(2,555);
-        for (int i=0;i<30;i++)
-        b.atDelete(i);
+        b.atDelete(4);
         b.at(3);
     }
 }
