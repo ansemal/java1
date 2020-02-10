6a3cac70c6a9dfbd341f26c4db9aedca2dec3cba
Sergei <ansemal@yandex.ru>
2020-02-10 20:19:00 +0200

    H6 Домашнее задание

--- src/ru/progwards/java1/lessons/interfaces/CalculateFibonacci.java
+++ src/ru/progwards/java1/lessons/interfaces/CalculateFibonacci.java
@@ -9,36 +9,26 @@ public class CalculateFibonacci {
     }
 
     public static int fiboNumber(int n) {
-       CacheInfo tempFibo = new CacheInfo();
-//        tempFibo = getLastFibo();
-//       if (tempFibo.n == n)
-//           return tempFibo.fibo;
+        if (lastFibo == null)
+            lastFibo = new CacheInfo();
+        if (lastFibo.n == n) return lastFibo.fibo;
         int a = 0;
         int b = 1;
         int c = 0;
         if (n == 1) {
-//            lastFibo.n = 1;
-            tempFibo.n = 1;
-            tempFibo.fibo = 1;
-            lastFibo = tempFibo;
-            return 1;
+            lastFibo.n = 1;
+            return lastFibo.fibo = 1;
         }
         for (int i = 2; i <= n; i++) {
             c = a + b;
             a = b;
             b = c;
         }
-//        lastFibo.n = n;
-        tempFibo.n = n;
-        tempFibo.fibo = c;
-        lastFibo = tempFibo;
-//        lastFibo.n = tempFibo.n;
-//        lastFibo.fibo = tempFibo.fibo;
-        return  c;
+        lastFibo.n = n;
+        return lastFibo.fibo = c;
     }
 
     public static CacheInfo getLastFibo() {
-//        lastFibo.n =1;
          return lastFibo;
     }
 
@@ -47,16 +37,13 @@ public class CalculateFibonacci {
     }
 
     public static void main(String[] args) {
-//       CacheInfo ff = new CacheInfo();
-//       ff.n = 5;
-//       ff.fibo = 25;
-//       CalculateFibonacci.lastFibo.n = ff.n;
-//       CalculateFibonacci.lastFibo.fibo = 0;
         System.out.println(fiboNumber(9));
         System.out.println(fiboNumber(25));
         System.out.println(getLastFibo());
-  //      clearLastFibo();
         System.out.println( lastFibo.n + ", " + lastFibo.fibo);
+        clearLastFibo();
+        System.out.println(fiboNumber(12));
+        System.out.println(fiboNumber(3));
 
     }
 
