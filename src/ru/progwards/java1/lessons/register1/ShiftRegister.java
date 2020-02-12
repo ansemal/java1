3d3484a2d0ce04bf6722b61268deda22725c8df0
Sergei <ansemal@yandex.ru>
2020-02-12 19:45:42 +0200

    A5.1 Электроника в беззнаковых числах

--- src/ru/progwards/java1/lessons/register1/ShiftRegister.java
+++ src/ru/progwards/java1/lessons/register1/ShiftRegister.java
@@ -4,17 +4,17 @@ public class ShiftRegister {
     public static void left(ByteRegister value) {
         for (int i=7; i > 0; i--)
             value.bitArrays[i] = value.bitArrays[i - 1];
-        value.bitArrays [0].value = false;
+        value.bitArrays [0] = value.bit0;
     }
 
     public static void right(ByteRegister value) {
         for (int i =0; i<7; i++)
             value.bitArrays[i] = value.bitArrays[i+1];
-        value.bitArrays [7].value = false;
+        value.bitArrays [7] = value.bit0;
     }
 
     public static void main(String[] args) {
-        ByteRegister aaa = new ByteRegister ((byte) 156);
+        ByteRegister aaa = new ByteRegister ((byte) 1);
         left(aaa);
         right(aaa);
     }
