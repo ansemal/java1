8d2ebad7341539d87d397ff3fabfbeaa686bdc57
Sergei <ansemal@yandex.ru>
2020-02-21 14:19:04 +0200

    H9 Домашнее задание

--- src/ru/progwards/java1/lessons/bigints/ArrayInteger.java
+++ src/ru/progwards/java1/lessons/bigints/ArrayInteger.java
@@ -20,7 +20,7 @@ public class ArrayInteger {
             value = value.divide(temp);
             System.out.print(digits[i]);
             i++;
-        } while (/*value.compareTo(temp) >= 0 && */i < digits.length);
+        } while (i < digits.length);
         System.out.println();
     }
 
@@ -35,12 +35,17 @@ public class ArrayInteger {
     boolean add(ArrayInteger num) {
         byte perenos = 0;
         byte perenosTemp;
-        for (byte i=0; i<this.digits.length; i++) {
+        int bolshe = 0;
+        if (this.digits.length > num.digits.length) {
+            bolshe = this.digits.length - num.digits.length;
+        }
+        for (byte i=0; i<this.digits.length-bolshe; i++) {
             if ((this.digits [i] + num.digits [i] + perenos) > 9) {
-                if (i == this.digits.length-1) {
+                if (i == this.digits.length-1 || this.digits.length < num.digits.length) {
                     Arrays.fill(this.digits, (byte) 0);
                     return false;
                 }
+
                 perenosTemp = (byte) ((this.digits [i] +  num.digits [i] + perenos) / 10);
                 this.digits [i] = (byte) ((this.digits [i] + num.digits [i] + perenos) % 10);
                 perenos = perenosTemp;
@@ -49,21 +54,27 @@ public class ArrayInteger {
                 this.digits[i] = (byte) ((this.digits[i] + num.digits[i] + perenos) % 10);
                 perenos = 0;
             }
-
         }
-
-
+        if (this.digits.length > num.digits.length && perenos > 0) {
+            for (int q=this.digits.length-bolshe; q < this.digits.length; q++) {
+                this.digits[q] = (byte) (this.digits [q] + perenos);
+                if (this.digits[q] < 9) {
+                    break;
+                }
+            }
+        }
         return true;
     }
 
     public static void main(String[] args) {
-        ArrayInteger aaa = new ArrayInteger(15);
-//        ArrayInteger bbb = new ArrayInteger(8);
-        BigInteger big = new BigInteger("105413504");
+        ArrayInteger aaa = new ArrayInteger(6);
+        ArrayInteger bbb = new ArrayInteger(4);
+        BigInteger big = new BigInteger("555555");
+        BigInteger big1 = new BigInteger("5555");
         aaa.fromInt(big);
- //       aaa.fromInt(big);
+        bbb.fromInt(big1);
         System.out.println(aaa.toInt());
- //       System.out.println(aaa.add(bbb));
+        System.out.println(aaa.add(bbb));
         System.out.println(aaa.toInt());
     }
 }
