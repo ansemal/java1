cb2d2d23560c52a1d07bd726f3df5905a27ce5b7
Sergei <ansemal@yandex.ru>
2020-02-25 00:12:32 +0200

    H10 Домашнее задание

---
+++ src/ru/progwards/java1/lessons/io1/Coder.java
@@ -0,0 +1,69 @@
+package ru.progwards.java1.lessons.io1;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.Scanner;
+
+public class Coder {
+    public static void codeFile(String inFileName, String outFileName, char[] code, String logName) {
+        try {
+            // чтение из файла
+            FileReader reader = new FileReader(inFileName);
+            Scanner scanner = new Scanner(reader);
+            // шифрование символов
+            try {
+                String temp;
+                char shifr;
+                while (scanner.hasNextLine()) {
+                    String str = "";
+                    temp = scanner.nextLine();
+                for (int q = 0; q<temp.length(); q++) {
+                    shifr = code[(int) temp.charAt(q)];
+                    str = str + shifr;
+                }
+                // запись в файл
+                    try {
+                        FileWriter writer = new FileWriter(outFileName, true);
+                        try {
+                            writer.write(str + "\n");
+                        } finally {
+                            writer.close();
+                        }
+                    } catch (Exception e) {
+                        logFile(e.getMessage(), logName);
+                    }
+                }
+            }finally {
+                reader.close();
+            }
+        } catch (Exception e) {
+            logFile(e.getMessage(), logName);
+        }
+    }
+
+    public static void logFile (String exc, String logName) {
+        //запись исключений в логфайл
+        try {
+            FileWriter log = new FileWriter(logName);
+            try {
+                log.write(exc);
+            }
+            finally {
+                log.close();
+            }
+        }  catch (Exception e) {
+            System.out.println("Всё пропало " + e.getMessage());
+        }
+
+    }
+
+    public static void main(String[] args) {
+        char[] code = new char [100];
+        int j=99;
+        for (int i=0; i<100; i++) {
+            code[i] = (char) j;
+            j-=i;
+        }
+        codeFile("tmp.txt", "out.txt", code, "log.txt");
+    }
+}
---
+++ src/ru/progwards/java1/lessons/io1/LineCount.java
@@ -0,0 +1,30 @@
+package ru.progwards.java1.lessons.io1;
+
+import java.io.FileReader;
+import java.util.Scanner;
+
+public class LineCount {
+    public static int calcEmpty(String fileName) {
+        int i = 0;
+        try {
+            FileReader reader = new FileReader(fileName);
+            Scanner scanner = new Scanner(reader);
+            try {
+                while (scanner.hasNextLine()) {
+                    if (scanner.nextLine().isEmpty()) {
+                        i++;
+                    }
+                }
+            } finally {
+                reader.close();
+            }
+            return i;
+        } catch (Exception e) {
+            return -1;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(calcEmpty("???"));
+    }
+}
