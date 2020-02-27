54d64f2293a142c3b29f5956d019655d1b456d07
Sergei <ansemal@yandex.ru>
2020-02-27 19:42:14 +0200

    H10 Домашнее задание

--- src/ru/progwards/java1/lessons/io1/Coder.java
+++ src/ru/progwards/java1/lessons/io1/Coder.java
@@ -18,7 +18,8 @@ public class Coder {
                     String str = "";
                     temp = scanner.nextLine();
                 for (int q = 0; q<temp.length(); q++) {
-                    shifr = code[(int)temp.charAt(q)];
+                    shifr = code [Character.getNumericValue(temp.charAt(q))];
+          //          shifr = code[(int)temp.charAt(q)];
                     str = str + shifr;
                 }
                 // запись в файл
@@ -26,7 +27,7 @@ public class Coder {
                         FileWriter writer = new FileWriter(outFileName, true);
                         try {
                             if (scanner.hasNextLine())
-                                writer.write(str + "\n");
+                                writer.write(str + /*code[32]); //*/"\n");
                             else
                                 writer.write(str);
                         } finally {
