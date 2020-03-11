cbf213731fdef51703285a520419a3d2f72aaf3e
Sergei <ansemal@yandex.ru>
2020-03-11 23:01:02 +0200

    H11 Домашнее задание

--- src/ru/progwards/java1/lessons/io2/Censor.java
+++ src/ru/progwards/java1/lessons/io2/Censor.java
@@ -15,7 +15,7 @@ public class Censor {
 
         @Override
         public String toString () {
-            return fileName + " : " + msg;
+            return fileName + ":" + msg;
         }
     }
 
