aa5cffdfc71a2abe1e54ba0869e8e15412bb4542
Sergei <ansemal@yandex.ru>
2020-03-16 01:17:40 +0200

    H12 Домашнее задание

--- src/ru/progwards/java1/lessons/collections/Creator.java
+++ src/ru/progwards/java1/lessons/collections/Creator.java
@@ -20,7 +20,7 @@ public class Creator {
     public static Collection<Integer> fillOdd(int n) {
         LinkedList <Integer> list = new LinkedList<>();
         int nechet = 1;
-        for (int i = 0; i <= n*3-1; i++) {
+        for (int i = 0; i <= n-1; i++) {
             list.push(nechet);
             nechet += 2;
         }
@@ -31,8 +31,8 @@ public class Creator {
         List<Integer> list = new ArrayList<>(n*3);
         for (int i = 0; i <= n*3-1; i+=3) {
             list.add(i, i);
-            list.add(i+1, (i+1)*(i+1));
-            list.add(i+2, (i+2)*(i+2));
+            list.add(i+1, i*i);
+            list.add(i+2, i*i*i);
         }
         return list;
     }
