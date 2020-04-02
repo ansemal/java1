d8eac9c6d5266da79499fa5c41e83d0a8c10a8a8
Sergei <ansemal@yandex.ru>
2020-04-02 23:31:09 +0200

    H17 Домашнее задание

--- src/ru/progwards/java1/lessons/files/OrderProcessor.java
+++ src/ru/progwards/java1/lessons/files/OrderProcessor.java
@@ -17,9 +17,13 @@ public class OrderProcessor{
 
     public OrderProcessor(String startPath) {
         this.startPath = startPath;
+        System.out.println("startPath " + startPath);
     }
 
     public int loadOrders(LocalDate start, LocalDate finish, String shopId) throws IOException {
+        System.out.println("start " + start.toString());
+        System.out.println("finish " + finish.toString());
+        System.out.println("shopID " + shopId );
         errorsCount = 0;
         PathMatcher pmShopID;
         PathMatcher pathMatcher = FileSystems.getDefault().getPathMatcher("glob:**/???-??????-????.csv");
@@ -37,6 +41,7 @@ public class OrderProcessor{
             public FileVisitResult visitFile (Path path, BasicFileAttributes attrs) {
                 if (pathMatcher.matches(path)) {
                     if (pmShopID.matches(path)) {
+                        System.out.println("зашёл в pmShopID");
                         LocalDate dateFile = LocalDate.ofInstant(attrs.lastModifiedTime().toInstant(), ZoneId.systemDefault());
                         // попадание в диапазон дат
                         if ((start == null || start.isBefore(dateFile) || start.equals(dateFile)) && (finish == null || finish.isAfter(dateFile) || finish.equals(dateFile))) {
@@ -45,13 +50,18 @@ public class OrderProcessor{
                                 List<String> orderItemStrings = Files.readAllLines(path);
                                 for (String orderItemString: orderItemStrings) {
                                     csv = orderItemString.split(", ");
+                                    System.out.println("csv " + Arrays.toString(csv));
                                     try {
                                         OrderItem orderItem = new OrderItem();
                                         orderItem.price = Double.parseDouble(csv[2]);
+                                        System.out.println("orderItem.price " + orderItem.price);
                                         orderItem.count = Integer.parseInt(csv[1]);
+                                        System.out.println("orderItem.count " + orderItem.count);
                                         orderItem.googsName = csv[0];
+                                        System.out.println("orderItem.googsName " + orderItem.googsName);
                                         orderItems.add(orderItem);
                                         sum += orderItem.price;
+                                        System.out.println("sum " + sum);
                                     } catch (Exception e) {
                                         System.out.println("В одной из строк файла " + path.getFileName() + "проблема с числами");
                                     }
@@ -59,13 +69,19 @@ public class OrderProcessor{
                                 // наполнение списка orders
                                 Order order = new Order();
                                 order.shopId = path.getFileName().toString().substring(0,3);
+                                System.out.println("order.shopId " + order.shopId);
                                 order.orderId = path.getFileName().toString().substring(4,10);
+                                System.out.println("order.orderId " + order.orderId);
                                 order.customerId = path.getFileName().toString().substring(11,15);
+                                System.out.println("order.customerId " + order.customerId);
                                 order.datetime = LocalDateTime.ofInstant(attrs.lastModifiedTime().toInstant(), ZoneId.systemDefault());
+                                System.out.println("order.datetime " + order.datetime.toString());
                                 order.sum = sum;
+                                System.out.println("sum " + sum);
                                 Collections.sort(orderItems);
                                 order.items = orderItems;
                                 orders.push(order);
+                                System.out.println("положено в orders");
                             } catch (Exception e) {
                                 System.out.println(e.getMessage());
                             }
@@ -73,6 +89,7 @@ public class OrderProcessor{
                     }
                 } else
                     errorsCount++;  // подсчет файлов с ошибкой в формате
+                System.out.println("errorsCount " + errorsCount);
                 return FileVisitResult.CONTINUE;
             }
 
