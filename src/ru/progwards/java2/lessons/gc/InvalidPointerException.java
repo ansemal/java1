c92e3799eccf3dd532447c114517166269527a33
SAM <ansemal@yandex.ru>
2020-06-08 17:08:31 +0200

    H5 Homework

---
+++ src/ru/progwards/java2/lessons/gc/InvalidPointerException.java
@@ -0,0 +1,16 @@
+package ru.progwards.java2.lessons.gc;
+
+public class InvalidPointerException extends RuntimeException {
+    private int wrongMark;
+
+    InvalidPointerException (int wrongMark) {
+        this.wrongMark = wrongMark;
+    }
+
+    @Override
+    public String toString() {
+        return "InvalidPointerException{" +
+                "Неправильный указатель = " + wrongMark +
+                '}';
+    }
+}
---
+++ src/ru/progwards/java2/lessons/gc/OutOfMemoryException.java
@@ -0,0 +1,16 @@
+package ru.progwards.java2.lessons.gc;
+
+public class OutOfMemoryException extends RuntimeException {
+    private int size;
+
+    OutOfMemoryException (int size) {
+        this.size = size;
+    }
+
+    @Override
+    public String toString() {
+        return "OutOfMemoryException{" +
+                " Невозможно найти память размером = " + size +
+                '}';
+    }
+}
